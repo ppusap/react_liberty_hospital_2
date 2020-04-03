b68ca1030bd1041c8aad1db3e9706382e02ad0d7
Anurag Alladi <alladi.anurag47@gmail.com>
2020-04-02 23:14:16 -0500

    Header/index.js updated.

--- liberty/src/components/Header/index.js
+++ liberty/src/components/Header/index.js
@@ -34,3 +34,27 @@ const Header = () => {
             src="https://libertyhospitalfoundation.org/wp-content/uploads/2018/04/FinalAsset-1.png"
             alt=""
           />
+        </NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar className="nav-options">
+          <Nav navbar>
+            <NavbarText>Home</NavbarText>
+            <UncontrolledDropdown nav inNavbar>
+              <DropdownToggle nav caret>
+                Scholarship
+              </DropdownToggle>
+              <DropdownMenu right>
+                <DropdownItem>
+                  <Link to="/scholarship">New Application</Link>
+                </DropdownItem>
+                <DropdownItem>
+                  <Link to="/scholarship/applications">
+                    Review Applications
+                  </Link>
+                </DropdownItem>
+                <DropdownItem divider />
+                <DropdownItem>
+                  <Link to="/scholarship/status">Check Status</Link>
+                </DropdownItem>
+              </DropdownMenu>
+            </UncontrolledDropdown>  
