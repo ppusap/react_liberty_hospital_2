316c29381e9236629a32fc8b5c3e429c9bdc8116
Midhunkandakatla <midhunkandakatla143@gmail.com>
2020-04-14 23:29:52 -0500

    Updating Hughes Family.js

--- liberty/src/components/Program/Family.js
+++ liberty/src/components/Program/Family.js
@@ -5,3 +5,14 @@ import { familyCrisis } from "./familyCrisis";
 import { get, update, create, download } from "api/program";
 import { Helmet } from "react-helmet";
 import { numberChange } from "lib/number";
+
+export default function Family() {
+    const [key, setKey] = useState(+new Date());
+    const [initial, setInitial] = useState({
+      crisis: familyCrisis,
+      no_of_unduplicated: null,
+      additional_services: null,
+      story_patient: null,
+      patient_contact: null,
+      type: "family"
+    });
