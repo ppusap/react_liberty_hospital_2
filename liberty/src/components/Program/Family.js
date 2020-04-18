25000d360e9b54ba0bb1d7bad99de7f158e04f7a
Midhunkandakatla <midhunkandakatla143@gmail.com>
2020-04-17 20:51:41 -0500

    Changes in Hughes Family.js

--- liberty/src/components/Program/Family.js
+++ liberty/src/components/Program/Family.js
@@ -7,44 +7,43 @@ import { Helmet } from "react-helmet";
 import { numberChange } from "lib/number";
 
 export default function Family() {
-    const [key, setKey] = useState(+new Date());
-    const [initial, setInitial] = useState({
-      crisis: familyCrisis,
-      no_of_unduplicated: null,
-      additional_services: null,
-      story_patient: null,
-      patient_contact: null,
-      type: "family"
-    });
-    const [submitting, setSubmitting] = useState(null);
+  const [key, setKey] = useState(+new Date());
+  const [initial, setInitial] = useState({
+    crisis: familyCrisis,
+    no_of_unduplicated: null,
+    additional_services: null,
+    story_patient: null,
+    patient_contact: null,
+    type: "family"
+  });
+  const [submitting, setSubmitting] = useState(null);
 
-    useEffect(() => {
-      async function getData() {
-        let res = await get("family");
-        if (Object.keys(res.data).length) {
-          setInitial({ ...res.data, type: "family" });
-          setKey(+new Date());
-        }
+  useEffect(() => {
+    async function getData() {
+      let res = await get("family");
+      if (Object.keys(res.data).length) {
+        setInitial({ ...res.data, type: "family" });
+        setKey(+new Date());
       }
-  
-      getData();
-    }, []);
+    }
 
-    const onSubmit = async values => {
-      setSubmitting(true);
-      if (initial.id) {
-        let res = await update(values);
-      } else {
-        let res = await create(values);
-      }
-      setSubmitting(false);
-    };
+    getData();
+  }, []);
 
-    return (
-      <div className="patient-assistance">
-        <Helmet>
-          <title>Liberty Hospital Foundation - Hughes Family Assistance</title>
-        </Helmet>
-        <Header />
-  
-    
+  const onSubmit = async values => {
+    setSubmitting(true);
+    if (initial.id) {
+      let res = await update(values);
+    } else {
+      let res = await create(values);
+    }
+    setSubmitting(false);
+  };
+
+  return (
+    <div className="patient-assistance">
+      <Helmet>
+        <title>Liberty Hospital Foundation - Hughes Family Assistance</title>
+      </Helmet>
+      <Header />
+      <div className="main-container" key={key}>
