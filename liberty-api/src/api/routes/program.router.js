78595158f350e4d604e88e2ea3853cca8ef7681d
milindar <milindarreddy.1996@gmail.com>
2020-04-18 20:44:51 -0500

    updated program.router.js

--- liberty-api/src/api/routes/program.router.js
+++ liberty-api/src/api/routes/program.router.js
@@ -9,3 +9,15 @@ const {
 } = require("../../controllers/program.controller");
 
 const router = express.Router();
+
+router.route("/:type").get(get);
+router.route("/:type/download").get(download);
+router.route("/:type").put(update);
+router.route("/:type").post(create);
+router.route("/").get(list);
+// router.route("/").post(create);
+// router.route("/:id").get(get);
+// router.route("/:id").put(update);
+// router.route("/:id").delete(deleteGrant);
+
+module.exports = router;
