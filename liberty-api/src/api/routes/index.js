eb1c9d27b6c39457cb50dcc9f994f6de048efd93
milindar <milindarreddy.1996@gmail.com>
2020-04-20 14:41:58 -0500

    updated index.js

--- liberty-api/src/api/routes/index.js
+++ liberty-api/src/api/routes/index.js
@@ -6,3 +6,16 @@ const programRoutes = require("./program.router");
 const { createPDF } = require("../../helpers/pdf");
 
 const router = express.Router();
+
+router.get("/", (req, res) => {
+    createPDF();
+    res.send("Hell");
+  });
+  
+  router.use("/auth", authRoutes);
+  router.use("/grant", grantRoutes);
+  router.use("/program", programRoutes);
+  router.use("/scholarship", scholarshipRoutes);
+  
+  module.exports = router;
+  
