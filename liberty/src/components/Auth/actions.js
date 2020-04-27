1581f2538c981ea87998e44936c9a017248b030f
Anurag Alladi <alladi.anurag47@gmail.com>
2020-04-27 14:04:31 -0500

    actions.js is updated

---
+++ liberty/src/components/Auth/actions.js
@@ -0,0 +1,24 @@
+import { createNamespacer } from "lib/reducers";
+import axiosInstance from "api";
+import { login as loginCall } from "api/login";
+
+const authNameSpacer = createNamespacer("AUTH");
+
+export const login = async (dispatch, data) => {
+  try {
+    const res = await loginCall(data);
+    let access_token = `Bearer ${res.data.access_token}`;
+    axiosInstance.defaults.headers.common["Authorization"] = access_token;
+    localStorage.access_token = access_token;
+    localStorage.auth_details = JSON.stringify(res.data);
+    dispatch({
+      type: authNameSpacer("SET_LOGGED_IN"),
+      payload: {
+        isLoggedIn: true,
+        access_token
+      }
+    });
+  } catch (ex) {
+    throw ex;
+  }
+};
