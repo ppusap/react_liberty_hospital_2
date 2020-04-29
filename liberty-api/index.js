d3a39219b460b4a68450464c1ebbb5e9b732859c
Rishika Reddy <46687612+rishikareddygaddam@users.noreply.github.com>
2020-04-29 05:24:08 -0500

    index.js

---
+++ liberty-api/index.js
@@ -0,0 +1,9 @@
+const app = require("./src/config/express");
+const PORT = process.env.PORT || 4000;
+const models = require("./src/api/models");
+
+app
+
+app.listen(PORT, () => {
+  console.log("Listening at Port " + PORT);
+});
