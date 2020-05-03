7fbf6c982ca62b83563679d25844a6551b864895
Pratyusha Pusapati <pratyushavarma.p@gmail.com>
2020-05-02 19:52:30 -0500

    updates liberty

--- liberty/index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./src/config/express");
-const PORT = process.env.PORT || 4000;
-const models = require("./src/api/models");
-
-app.listen(PORT, () => {
-  console.log("Listening at Port " + PORT);
-});
